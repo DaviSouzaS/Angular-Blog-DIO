0b59ad7f3abf28109d06f5811d5d22a3ea2d3a28
davi <daavisouza.santana@gmail.com>
2023-09-16 19:25:57 -0300

    feat: create news page

--- src/app/pages/news-page/news-page.component.ts
+++ src/app/pages/news-page/news-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { data } from 'src/app/mock/data';
 
 @Component({
   selector: 'app-news-page',
@@ -7,4 +9,39 @@ import { Component } from '@angular/core';
 })
 export class NewsPageComponent {
 
+  @Input()
+  id: number = 0
+
+  @Input()
+  name: string = ""
+
+  @Input()
+  date: string = ""
+
+  @Input()
+  description: string = ""
+
+  @Input()
+  image: string = ""
+
+  constructor(private route:ActivatedRoute){}
+
+  ngOnInit() {
+    
+    this.route.paramMap.subscribe(params => {
+      const id = params.get('id');
+      this.id = Number(id)
+    })
+
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id: number){
+    const dataPage = data.filter(newsPage => newsPage.id === id)[0]
+
+    this.description = dataPage.description
+    this.date = dataPage.date
+    this.image = dataPage.image
+    this.name = dataPage.name
+  }
 }
